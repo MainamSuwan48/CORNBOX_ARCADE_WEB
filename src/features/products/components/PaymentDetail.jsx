f4c7e6e38b161c7011bd2281a91545c769832988
MainamSuwan48 <112855044+SexyCornman@users.noreply.github.com>
2024-02-14 16:41:20 +0700

    refactored payment detail into component

--- src/features/products/components/PaymentDetail.jsx
+++ src/features/products/components/PaymentDetail.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import ActionButton from "../../../components/ui/ActionButton";
+import QrPic from "../../../assets/QR.png";
 
 function PaymentDetail() {
-  return <div>PaymentDetail</div>;
+  return (
+    <div className="flex flex-col p-8 bg-black w-96 rounded-none rounded-xl justify-center items-center gap-2">
+      <div>
+        <img src={QrPic}></img>
+      </div>
+      <div className="text-white  text-xl">
+        <p>Scan the QR code to pay</p>
+      </div>
+      <div className="text-primary  text-xl">
+        <p>Please Deposit : 1500 THB</p>
+      </div>
+      <input
+        className="bg-transparent  border border-input border-primary bg-primary rounded-lg p-2 w-60"
+        type="file"
+      ></input>
+      <ActionButton>CHECKOUT</ActionButton>
+    </div>
+  );
 }
 
 export default PaymentDetail;
--- src/pages/layouts/CheckOutLayout.jsx
+++ src/pages/layouts/CheckOutLayout.jsx
@@ -1,29 +1,13 @@
 import React from "react";
-import ShoppingCart from "../../features/products/components/ShoppingCart";
 import CheckOutDetail from "../../features/products/components/CheckOutDetail";
-import QrPic from "../../assets/QR.png";
-import ActionButton from "../../components/ui/ActionButton";
+import PaymentDetail from "../../features/products/components/PaymentDetail";
+
 
 function CheckOutLayout() {
   return (
     <div className="flex h-with_header mt-20 flex-1 justify-center items-start p-8 gap-8">
       <CheckOutDetail />
-      <div className="flex flex-col p-8 bg-black w-96 rounded-none rounded-xl justify-center items-center gap-2">
-        <div>
-          <img src={QrPic}></img>
-        </div>
-        <div className="text-white  text-xl">
-          <p>Scan the QR code to pay</p>
-        </div>
-        <div className="text-primary  text-xl">
-          <p>Please Deposit : 1500 THB</p>
-        </div>
-        <input
-          className="bg-transparent  border border-input border-primary bg-primary rounded-lg p-2 w-60"
-          type="file"
-        ></input>
-        <ActionButton>CHECKOUT</ActionButton>
-      </div>
+      <PaymentDetail />
     </div>
   );
 }
