8b52b8415d76bfe513c1cc814daf3aeac7a96a35
MainamSuwan48 <112855044+SexyCornman@users.noreply.github.com>
2024-02-15 02:04:01 +0700

    added login and register validators

---
+++ src/features/auth/validations/validate-login.js
@@ -0,0 +1,17 @@
+import Joi from "joi";
+import validate from "../../../utils/validator";
+
+const loginSchema = Joi.object({
+  usernameOrEmail: Joi.string().email().required().messages({
+    "string.empty": "Please enter your username or email",
+    "any.required": "Please enter your username or email",
+  }),
+  password: Joi.string().min(6).required().messages({
+    "string.empty": "Please enter your password",
+    "string.min": "Password must be at least 6 characters",
+    "any.required": "Please enter your password",
+  }),
+});
+
+const validateLogin = (input) => validate(loginSchema)(input);
+export default validateLogin;
