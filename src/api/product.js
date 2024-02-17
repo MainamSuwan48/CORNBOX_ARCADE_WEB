a02d6b2e1e6c53e4141342cbad11aa78a88115c3
MainamSuwan48 <112855044+SexyCornman@users.noreply.github.com>
2024-02-18 00:40:28 +0700

    added api for cart

--- src/api/product.js
+++ src/api/product.js
@@ -2,3 +2,11 @@ import axios from "../config/axios";
 
 export const getAllProducts = () => axios.get("/product");
 export const getProductById = (id) => axios.get(`/product/${id}`);
+export const createCart = (data) => axios.post("/cart/create", data);
+export const getCartByUserIdData = (data) => axios.post("/cart/", data); 
+export const addItemToCart = (data) => axios.post("/cart/add", data);
+export const updateCartItem = (data) => axios.put("/cart/item", data);
+export const deleteCartItem = (data) => axios.delete("/cart/item", { data });
+
+
+
