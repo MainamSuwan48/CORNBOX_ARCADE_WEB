1a81eddd4a2af894bdd214f73bd8657150a2c8c3
MainamSuwan48 <112855044+SexyCornman@users.noreply.github.com>
2024-02-21 22:50:02 +0700

    fixed problem when there's no address

--- src/features/user/components/ShippingAddressInProfile.jsx
+++ src/features/user/components/ShippingAddressInProfile.jsx
@@ -8,23 +8,22 @@ import ShippingAddressSelector from "./ShippingAddressSelector";
 
 function ShippingAddressInProfile() {
   const [isLoading, setIsLoading] = useState(true);
-  const { shippingAddress, address } = useUser();
+  const { shippingAddress, addresses } = useUser();
 
   useEffect(() => {
     if (shippingAddress) {
       setIsLoading(false);
     }
-  }, [shippingAddress]);
+  }, [shippingAddress, addresses]);
 
   return isLoading ? (
     <div>loading...</div>
-  ) : (
+  ) : addresses.length === 0 ? "create some address first": (
     <div className="flex justify-start items-start m-2 gap-2">
       <div className="flex flex-col justify-between gap-2 items-start text-2xl">
         <p className="text-2xl text-white ml-2">Your Shipping Address</p>
         <ShippingAddressSelector />
       </div>
-
       <AddressSingular address={shippingAddress} type={"shipping"} />
     </div>
   );
