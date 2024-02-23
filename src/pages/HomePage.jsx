852556b684e3908f73a1fb12378dc5a91805666e
MainamSuwan48 <112855044+SexyCornman@users.noreply.github.com>
2024-02-23 14:22:15 +0700

    added second hero

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -30,6 +30,7 @@ function HomePage() {
           loop
           muted
           style={{
+            opacity: "0.5",
             position: "absolute",
             width: "100%",
             height: "100%",
@@ -49,6 +50,42 @@ function HomePage() {
           <img src="https://i.imgur.com/LgoOidy.png"></img>
         </div>
       </div>
+
+      <div
+className="backdrop-blur-lg flex items-center justify-center w-full h-screen bg-base-300"
+style={{ position: "relative", width: "100vw", height: "80vh" }}
+>
+<video
+  className="-z-50"
+  autoPlay
+  loop
+  muted
+  style={{
+    opacity: "0.5",
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+  }}
+>
+  <source src="https://res.cloudinary.com/dhm6pitfd/video/upload/v1708671752/GUILTY_GEAR_-STRIVE-_Available_Now_with_Xbox_Game_Pass_mhxkcg.webm" />
+</video>
+<div className="absolute right-0 flex text-9xl tracking-tighter z-50 p-5 p-8">
+  <div>
+    <div className="font-bold text-base-100 ">YOUR</div>
+    <div className="font-bold text-base-100">GAME</div>
+  </div>
+  <div className="font-black text-primary">DOMINATE</div>
+</div>
+<div
+  style={{ transform: "scaleX(-1)" }}
+  className="transition-all left-0 absolute w-1/2 r:z-50"
+>
+  <img src="https://www.guiltygear.com/ggst/en/wordpress/wp-content/uploads/2020/09/chara_pc_ram.webp"></img>
+</div>
+</div>
+    
+    
     </>
   );
 }
